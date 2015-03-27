3234c875cab0c35c05fda170c0d7e702d268ade1
Roberts Ellis <c9roberu@cdf.toronto.edu>
2015-03-27 17:45:17 -0400

    anti-aliasing done

--- raytracerLinux/raytracer.cpp
+++ raytracerLinux/raytracer.cpp
@@ -244,16 +244,24 @@ void Raytracer::render( int width, int height, Point3D eye, Vector3D view,
 	initPixelBuffer();
 	viewToWorld = initInvViewMatrix(eye, view, up);
 
+        
+        //number of points being sampled
+        int sample = 16;
+
 	// Construct a ray for each pixel.
+        //anti-aliasing - construct multiple rays using random sampling and average them to remove fragments
 	for (int i = 0; i < _scrHeight; i++) {
 		for (int j = 0; j < _scrWidth; j++) {
-		
-			// Sets up ray origin and direction in view space, 
-			// image plane is at z = -1.
+                    Colour col;
+                    for (float pixeli = i; pixeli < i + 1; pixeli = pixeli + .25)  {
+
+		        for (float pixelj = j; pixelj < j + 1; pixelj += .25) {
+		        // Sets up ray origin and direction in view space, 
+	                // image plane is at z = -1.
 			Point3D origin(0, 0, 0);
 			Point3D imagePlane;
-			imagePlane[0] = (-double(width)/2 + 0.5 + j)/factor;
-			imagePlane[1] = (-double(height)/2 + 0.5 + i)/factor;
+			imagePlane[0] = (-double(width)/2 + 0.5 + pixelj)/factor;
+			imagePlane[1] = (-double(height)/2 + 0.5 + pixeli)/factor;
 			imagePlane[2] = -1;
 			
 			// TODO: Convert ray to world space and call 
@@ -272,12 +280,15 @@ void Raytracer::render( int width, int height, Point3D eye, Vector3D view,
 			Ray3D ray(eye, pixelDirection);
 			ray.dir.normalize();
 			
-			Colour col = shadeRay(ray); 
-			
-			_rbuffer[i*width+j] = int(col[0]*255);
-			_gbuffer[i*width+j] = int(col[1]*255);
-			_bbuffer[i*width+j] = int(col[2]*255);
-		}
+                        //sum the colours from the different ray components
+			col = col + shadeRay(ray); 
+                        }
+		}	
+                //store the result of numerical integration into the pixel
+		_rbuffer[i*width+j] = int(col[0]*255/sample);
+		_gbuffer[i*width+j] = int(col[1]*255/sample);
+		_bbuffer[i*width+j] = int(col[2]*255/sample);
+                }
 	}
 
 	flushPixelBuffer(fileName);
